7d4367de254dbe2880f08a82d710463a82c4ed0e
ptawuringa <ptaruwinga@yahoo.com>
2020-10-26 15:43:29 +0200

    new data

--- index.js
+++ index.js
@@ -47,7 +47,6 @@ const connectionString = process.env.DATABASE_URL || 'postgresql://patience:code
 
 const pool = new Pool({
     connectionString,
- //   ssl: useSSL
 });
 const greeted = greeting(pool)
 
